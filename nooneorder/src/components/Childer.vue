432657c80561de481da3b86812922d2a2518a8b9
zhanglxlx <zhanglx@tedu.cn>
2019-08-21 18:58:53 +0800

    praent

--- nooneorder/src/components/Childer.vue
+++ nooneorder/src/components/Childer.vue
@@ -1,31 +1,41 @@
 <template>
     <div class="childer">
         <h1>我是子组件</h1>
-        <p>子组件接受的值---:{{title}}第二个参数： {{msg}}</p>
+        <!-- <p>子组件接受的值---:{{title}}第二个参数： {{msg}}</p> -->
+        <p></p>
         <button @click="run('123')">
             调用父组件传过来的方法 run
         </button>
         <button @click="getParent()">获取子组件传递过来的title</button>
+        <button @click="getparent()">子组件中自动获取父组件中的方法</button><br>
     </div>
 </template>
 <script>
     export default {
         data() {
             return {
-                
+                msgc:"我是一个子组件"
             }
         },
-        // props:['title','msg','run','home'], //接受子组件的属性名称
-        props:{
-            'title':String,
-            'msg':String,
-            'run':Function,
-            'home':Object
-        },
+        props:['title','msg','runc','home'], //接受子组件的属性名称
+        // props:{
+        //     'title':String,
+        //     'msg':String,
+        //     'run':Function,
+        //     'home':Object
+        // },
         methods: {
             getParent(){
                 alert(this.title);
-                alert(this.home.title);
+                alert(this.runc);
+                // alert(this.home.title);
+            },
+            getparent(){
+                console.log(this.$parent.msg)
+                console.log(this.$parent.runc())
+            },
+            run(){
+                alert("我是子组件的方法")
             }
         },
         mounted() {
--- nooneorder/src/components/Parent.vue
+++ nooneorder/src/components/Parent.vue
@@ -5,38 +5,49 @@
             <p style="border:1px solid red"> 
                 1.父组件调用子组件的时候，绑定动态属性<br>
                      <!-- 自定义一个属性 -->
-                <children :title="title" :msg="msg" :run="run" :home="this"></children>
+                <childer :title="title" :msg="msg" :runc="runc"  :home="this"></childer>
                 2.在子组件里面通过  props接收父组件传过来的数据<br>
-                <!-- props:['title','msg','run','home'], -->
+                <!-- props:['title','msg','runc','home'], -->
                 3.直接在子组件里面使用<br>
             </p>
         <h3>二种：父组件主动获取子组件的数据和方法</h3>
             <p style="border:1px solid green">
-            <childre></childre>
+                1.在子组件中定义一个ref<br>
+            <childer ref="childre1"></childer><br>
+            <button @click="getChild">获取子组件中的方法run</button><br>
+                2.在父组件里面通过  this.$refs.childre1.要传的名字<br>
             </p>
         <h3>三种：子组件主动获取父组件的数据和方法</h3>
             <p  style="border:1px solid blue">
-
+                <!-- 1.this.$prent.数据 -->
             </p>
         
     </div>
 </template>
 <script>
-import children from './Childer.vue'
+import childer from './Childer.vue'
 export default {
     data() {
         return {
             title:"123456",
-            msg:"123"
+            msg:"父组件"
         }
     },
     methods: {
+        runc(){
+            alert("父组件传递的一个方法")
+        },
         run(data){
             alert("我是父组件的run方法"+data)
+        },
+        getChild(){
+            alert(this.$refs.childre1.msgc);
+            alert(this.$refs.childre1.run())
+
         }
     },
     components:{
-        children
+        childer
 
     }
 }
